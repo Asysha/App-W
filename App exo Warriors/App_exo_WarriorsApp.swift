43ff48b83e9b36c0a0b545b98d76cf33079a44a4
Solobolo <contact.marie.css@gmail.com>
2022-06-15 06:23:06 -0700

    Initial Commit

---
+++ App exo Warriors/App_exo_WarriorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_exo_WarriorsApp.swift
+//  App exo Warriors
+//
+//  Created by Apprenant 11 on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct App_exo_WarriorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
